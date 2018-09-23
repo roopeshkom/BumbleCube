edcd6f7e1c7ec56195bb6b86e8fa83c013f5e346
J. Liu <junze0407@gmail.com>
2018-09-23 14:44:02 -0400

    adding characters and animation on a separate plane

---
+++ Assets/_Scripts/player_controls/player_controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour {
+
+	private Animator animator;
+	private ani_control_script animationControl;
+	private CharacterController charController;
+	private float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		animationControl = GetComponent<ani_control_script>();
+		charController = GetComponent<CharacterController>();
+		speed = 0.1f;
+	}
+	
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        charController.Move(movement * speed);
+        Debug.Log(charController.velocity);
+        if (charController.velocity == Vector3.zero && !animator.GetBool("isIdle"))
+        {
+        	animationControl.Idle();           
+        }
+
+        if (charController.velocity != Vector3.zero && !animator.GetBool("isRun"))
+        {
+        	animationControl.Run();           
+        }
+
+    }
+}

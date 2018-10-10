8958682a1f66b6f430369ec44fa1f731850ed8b0
J. Liu <junze0407@gmail.com>
2018-10-09 23:34:51 -0400

    added jump logic to the player and remove the other char and the extra plane

--- Assets/_Scripts/player_controls/player_controller.cs
+++ Assets/_Scripts/player_controls/player_controller.cs
@@ -7,32 +7,39 @@ public class player_controller : MonoBehaviour {
 	private Animator animator;
 	private ani_control_script animationControl;
 	private CharacterController charController;
-	private float gravity = 0;
-	private float speed;
-	private bool isGrounded;
-
-    private readonly float m_interpolation = 10;
+	private float gravity = 10f;
+	private float jumpSpeed = 4.7f;
+	private float speed = 1.5f;
+	private bool isJumping = false;
+	private Vector3 moveDirection = Vector3.zero;
+	private float t = 0;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
 		animationControl = GetComponent<ani_control_script>();
 		charController = GetComponent<CharacterController>();
-		speed = 0.1f;
-		isGrounded = true;
 	}
 	
-    void FixedUpdate () {    	
-    	 gravity -= 9.81f * Time.deltaTime;
+    void FixedUpdate () {
+        if (charController.isGrounded) {
+        	isJumping = false;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
 
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-        Vector3 gravityV = new Vector3(0f, gravity, 0f);
-        transform.rotation = Quaternion.LookRotation (movement * speed);
-        charController.Move((movement + gravityV) * speed);
+            if (Input.GetButton("Jump")) {
+                isJumping = true;
+                t = 0;
+            }
+        }
+        if (isJumping) {
+        	moveDirection.y = jumpSpeed - gravity * t;
+        	t += Time.deltaTime;
+        } else {
+	        moveDirection.y -= gravity;
+        }
+        charController.Move(moveDirection * Time.deltaTime);
+        
       //   if (charController.velocity == Vector3.zero && !animator.GetBool("isIdle"))
       //   {
       //   	animationControl.Idle();           
@@ -45,50 +52,5 @@ public class player_controller : MonoBehaviour {
 
       //   }
     }
-
-    // private void TankUpdate() {
-    //     float v = Input.GetAxis("Vertical");
-    //     float h = Input.GetAxis("Horizontal");
-
-    //     bool walk = Input.GetKey(KeyCode.LeftShift);
-
-    //     if (v < 0) {
-    //         if (walk) { v *= m_backwardsWalkScale; }
-    //         else { v *= m_backwardRunScale; }
-    //     } else if(walk)
-    //     {
-    //         v *= m_walkScale;
-    //     }
-
-    //     float m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-    //     float m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
-
-    //     transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
-    //     transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
-
-    //     animator.SetFloat("MoveSpeed", m_currentV);
-
-    //     JumpingAndLanding();
-    // }
-
-    // private void JumpingAndLanding() {
-    //     bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
-
-    //     if (jumpCooldownOver && isGrounded && Input.GetKey(KeyCode.Space))
-    //     {
-    //         m_jumpTimeStamp = Time.time;
-    //         m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
-    //     }
-
-    //     if (!wasGrounded && isGrounded)
-    //     {
-    //         m_animator.SetTrigger("Land");
-    //     }
-
-    //     if (!isGrounded && wasGrounded)
-    //     {
-    //         animator.SetTrigger("Jump");
-    //     }
-    // }
 
 }

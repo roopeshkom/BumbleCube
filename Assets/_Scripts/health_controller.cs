eadbba485f85472604f71488294b6c09a64b7c4d
Roopeswar Kommalapati <roopeshkom@gmail.com>
2018-10-13 19:55:19 -0400

    Implemented basic shooting system with raycasts, and basic health component system

---
+++ Assets/_Scripts/health_controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health_controller : MonoBehaviour {
+
+    public int initialHealth;
+    private int health;
+    private TextMesh healthUI;
+
+    void Start()
+    {
+        health = initialHealth;
+        healthUI = gameObject.GetComponentInChildren<TextMesh>();
+    }
+
+    private void Update()
+    {
+        healthUI.text = health.ToString();
+
+        if (health <= 0){
+            Destroy(gameObject);
+        }
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+    }
+
+    public void Heal(int healing)
+    {
+        if(health+healing > initialHealth)
+        {
+            health = initialHealth;
+        }
+        else
+        {
+            health += healing;
+        }
+    }
+}
---
+++ Assets/_Scripts/player_controls/shooting_controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting_controller : MonoBehaviour {
+
+    Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+        mainCam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0)){
+            RaycastHit camHit;
+            Ray camRay = mainCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(camRay, out camHit, Mathf.Infinity))
+            {
+                Transform objectHit = camHit.transform;
+                health_controller objectHealth = objectHit.GetComponent<health_controller>();
+                if (objectHealth != null){
+                    objectHealth.Damage(35);
+                }
+            }
+        }
+
+    }
+}
